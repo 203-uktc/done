5a668b87ee4d64599fb193854fd3f255061528e3
ivayladr <20331@uktc-bg.com>
2025-01-16 09:42:56 +0200

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -4,7 +4,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Calculator {
-    // Create the frame
     private JFrame frame;
     private JTextField textField;
 
